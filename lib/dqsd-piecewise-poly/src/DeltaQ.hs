f403c507b55b4802936d615c6d6276f9f241d364
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-11-05 11:27:55 +0100

    Add parent module `DeltaQ`

---
+++ lib/dqsd-piecewise-poly/src/DeltaQ.hs
@@ -0,0 +1,13 @@
+{-|
+Copyright:
+    Predictable Network Solutions Ltd., 2024
+License: BSD-3-Clause
+Maintainer: neil.davies@pnsol.com
+-}
+module DeltaQ
+    ( module DeltaQ.Class
+    , module DeltaQ.PiecewisePolynomial
+    ) where
+
+import DeltaQ.Class
+import DeltaQ.PiecewisePolynomial
