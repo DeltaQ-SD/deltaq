78679148d2b5c70a41421e51a10ac2e5cd93b2d7
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-11-09 11:08:02 +0100

    Add tests for `uniform`

--- lib/dqsd-piecewise-poly/test/DeltaQ/PiecewisePolynomialSpec.hs
+++ lib/dqsd-piecewise-poly/test/DeltaQ/PiecewisePolynomialSpec.hs
@@ -8,7 +8,8 @@ module DeltaQ.PiecewisePolynomialSpec
 import Prelude
 
 import DeltaQ.Class
-    ( Outcome (..)
+    ( DeltaQ (..)
+    , Outcome (..)
     , (.>>.)
     , (./\.)
     , (.\/.)
@@ -48,6 +49,14 @@ spec = do
         xit ".\\/." $ property $
             \(NonNegative t) (NonNegative s) ->
                 (wait t .\/. wait s) .=== wait (min t s)
+
+    describe "uniform" $ do
+        xit "wait .>>. uniform" $ property $
+            \(NonNegative r) (NonNegative s) (NonNegative t) ->
+                (wait t .>>. uniform r s) .=== uniform (t+r) (t+s)
+        xit "uniform .>>. wait" $ property $
+            \(NonNegative r) (NonNegative s) (NonNegative t) ->
+                (uniform r s .>>. wait t) .=== uniform (r+t) (s+t)
 
 {-----------------------------------------------------------------------------
     Random generators
