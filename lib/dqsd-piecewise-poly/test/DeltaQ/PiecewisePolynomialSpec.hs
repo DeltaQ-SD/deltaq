222e0d1dbd7b10df0aa62e687a5be8b8ffa4afa4
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-11-10 01:41:29 +0100

    Add random generator for `Durations Rational`

--- lib/dqsd-piecewise-poly/test/DeltaQ/PiecewisePolynomialSpec.hs
+++ lib/dqsd-piecewise-poly/test/DeltaQ/PiecewisePolynomialSpec.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module DeltaQ.PiecewisePolynomialSpec
     ( spec
@@ -7,6 +10,9 @@ module DeltaQ.PiecewisePolynomialSpec
 
 import Prelude
 
+import Data.Ratio
+    ( (%)
+    )
 import DeltaQ.Class
     ( DeltaQ (..)
     , Outcome (..)
@@ -24,15 +30,28 @@ import Test.Hspec
     , xit
     )
 import Test.QuickCheck
-    ( NonNegative (..)
+    ( Arbitrary
+    , Gen
+    , NonNegative (..)
+    , Positive (..)
     , Property
     , (===)
+    , arbitrary
+    , choose
+    , chooseInteger
+    , frequency
+    , getSize
+    , oneof
     , property
+    , scale
+    , vectorOf
     )
 
 {-----------------------------------------------------------------------------
     Tests
 ------------------------------------------------------------------------------}
+infix 0 .===
+
 -- | '(===)' with constrained types.
 (.===) :: Durations Rational -> Durations Rational -> Property
 (.===) = (===)
@@ -61,3 +80,53 @@ spec = do
 {-----------------------------------------------------------------------------
     Random generators
 ------------------------------------------------------------------------------}
+instance Arbitrary (Durations Rational) where
+    arbitrary = scale (`div` 10) genDeltaQ
+
+-- | Generate a random 'DeltaQ' by generating a random expression.
+genDeltaQ
+    :: (DeltaQ o, Arbitrary (Duration o), Probability o ~ Rational)
+    => Gen o
+genDeltaQ = do
+    size <- getSize
+    genDeltaQFromList =<< vectorOf size genSimpleOutcome
+
+-- | Generate a simple probability distribution using 'uniform'.
+genUniform :: (DeltaQ o, Arbitrary (Duration o)) => Gen o
+genUniform = do
+    NonNegative a <- arbitrary
+    Positive d <- arbitrary
+    pure $ uniform a (a + d)
+
+-- | Generate a deterministic outcome 'wait'.
+genWait :: (Outcome o, Arbitrary (Duration o)) => Gen o
+genWait = do
+    NonNegative a <- arbitrary
+    pure $ wait a
+
+-- | Generate a simple outcome — one of 'uniform', 'wait', or 'never'.
+genSimpleOutcome :: (DeltaQ o, Arbitrary (Duration o)) => Gen o
+genSimpleOutcome =
+    frequency [(20, genUniform), (4, genWait), (1, pure never)]
+
+-- | Generate a random probability between (0,1) an
+genProbability :: Gen Rational
+genProbability = do
+    denominator <- chooseInteger (1,2^(20 :: Int))
+    numerator <- chooseInteger (0, denominator)
+    pure (numerator % denominator)
+
+-- | Generate a random 'DeltaQ' by combining a given list
+-- of outcomes with random operations.
+genDeltaQFromList :: (DeltaQ o, Probability o ~ Rational) => [o] -> Gen o
+genDeltaQFromList [] = pure never
+genDeltaQFromList [x] = pure x
+genDeltaQFromList xs = do
+    n <- choose (1, length xs - 1)
+    let (ys, zs) = splitAt n xs
+    genOp <*> genDeltaQFromList ys <*> genDeltaQFromList zs
+  where
+    genChoice = do
+        p <- genProbability
+        pure $ choice p 
+    genOp = oneof [pure (.>>.), pure (./\.), pure (.\/.), genChoice]
