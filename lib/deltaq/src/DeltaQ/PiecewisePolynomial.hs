6feef22f05df6aec93bb359e95fec5f1b1162830
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-11-12 16:16:54 +0100

    Polish documentation

--- lib/deltaq/src/DeltaQ/PiecewisePolynomial.hs
+++ lib/deltaq/src/DeltaQ/PiecewisePolynomial.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 
+{-|
+Copyright:
+    Predictable Network Solutions Ltd., 2024
+License: BSD-3-Clause
+Maintainer: neil.davies@pnsol.com
+Description:
+    Instances via piecewise polynomials.
+-}
 module DeltaQ.PiecewisePolynomial
     ( Durations (..)
     ) where
