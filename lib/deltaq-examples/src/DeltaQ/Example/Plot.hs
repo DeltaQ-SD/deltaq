497612a92b7bff01a8c90f161a22018ab63905c9
Heinrich Apfelmus <apfelmus@quantentunnel.de>
2024-12-07 13:25:39 +0100

    Add package `deltaq-examples`

---
+++ lib/deltaq-examples/src/DeltaQ/Example/Plot.hs
@@ -0,0 +1,7 @@
+{-|
+Copyright   : Predictable Network Solutions Ltd., 2024
+License     : BSD-3-Clause
+Maintainer  : neil.davies@pnsol.com
+Description : Example plots
+-}
+module DeltaQ.Example.Plot where
